fd5a7374bca614e38493d0707a51355637ec22c9
Sreeraj Sreekumaran <SRSR@simcorp.com>
2024-10-08 16:35:51 +0800

    Created Homework1ConsoleApp and pushed to master version

---
+++ Homework1ConsoleApp/Homework1ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework1ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
